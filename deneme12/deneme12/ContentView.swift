dd1a8d17618a550d1fcc4dac567a4a56acae267e
HASAN BERAT GURBUZ <hasan.gurbuz@atmosware.turkcell.com.tr>
2024-01-31 11:02:26 +0300

    new file

---
+++ deneme12/deneme12/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  deneme12
+//
+//  Created by HASAN BERAT GURBUZ on 31.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ deneme12/deneme12/deneme12App.swift
@@ -0,0 +1,17 @@
+//
+//  deneme12App.swift
+//  deneme12
+//
+//  Created by HASAN BERAT GURBUZ on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct deneme12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
